a8dd44b50828f14aec7131fbffe27af74fa72927
Bruno Martins <bacm@live.co.uk>
2020-10-27 13:06:05 +0100

    Construction du tableau contenant les résultats

--- anagram.rb
+++ anagram.rb
@@ -1,10 +1,25 @@
+# we could use the benchmark lib instead
+start = Time.now
 groups = {}
+
+# hardcoded filename that should be passed by argument
 File.readlines('liste.de.mots.francais.frgut.txt').each do |str|
-  sorted = str.chars.sort.join
+  s = str.strip
+  sorted = s.chars.sort.join
   if groups.key?(sorted)
-    groups[sorted].append(str)
+    groups[sorted].append(s)
   else
-    groups[sorted] = [str]
+    groups[sorted] = [s]
   end
 end
-puts(groups)
+
+# build output array
+list = []
+groups.each do |key, arr|
+  list.append(arr)
+end
+
+finish = Time.now
+
+puts finish - start
+puts list.to_s
